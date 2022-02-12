b2d091665c6351f80eef2fe1678f280eaea44803
mer-police <73510753+mer-police@users.noreply.github.com>
2022-02-12 00:07:06 -0700

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -1,3 +1,28 @@
+getgenv().Aimbot.Settings = {
+    SendNotifications = true,
+    SaveSettings = true, -- Re-execute upon changing
+    ReloadOnTeleport = true,
+    Enabled = true,
+    TeamCheck = false,
+    AliveCheck = true,
+    WallCheck = false, -- Laggy
+    Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+    TriggerKey = "MouseButton2",
+    Toggle = false,
+    LockPart = "Head" -- Body part to lock on (Character part's name)
+}
+
+getgenv().Aimbot.FOVSettings = {
+    Enabled = true,
+    Visible = true,
+    Amount = 90,
+    Color = "255, 255, 255",
+    LockedColor = "255, 70, 70",
+    Transparency = 0.5,
+    Sides = 60,
+    Thickness = 1,
+    Filled = false
+}
 --// Preventing Multiple Processes
 
 pcall(function()
